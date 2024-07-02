47bf7e2ffbbd94d5494acd568f702684a39e97bf
LeonardoAlmeidaRibas <leo.almeidaribas21@gmail.com>
2024-07-02 20:09:11 -0300

    Fatorial

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-let numero = prompt(`Digite um algarismo: `)
+function ehNumero(num) {
+    return isNaN(Number(num))
+}
 
-let maior = numero.split('').sort((a, b) => b - a).join('');
+function maiorNumero(num) {
 
-console.log(`O Maior número possível com o seu algarismo é: ${maior}`)
+    if(ehNumero(num)) {
+        let array = num.split("");
+    
+        for (let i = 0; i < array.length; i++) {
+            for (let j = 0; j < array.length; j++) {
+                if(array[i] > array[j]) {
+                    let aux = array[j];
+                    array[j] = array[i];
+                    array[i] = aux;
+                }
+            }
+        }
+        return array.join("");
+    }
+    
+        console.log(`Númer o inválido`)
+        return false
+    }
+
+let numero = prompt(`Digite um algarismo: `);
+
+let maior = maiorNumero(numero);
+console.log(maior);
+
+// let maior = numero.split('').sort((a, b) => b - a).join('');
+
+// console.log(`O Maior número possível com o seu algarismo é: ${maior}`)
